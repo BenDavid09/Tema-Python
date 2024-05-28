f35fecbdd03a5f14ba004c5007fd167804e94306
BenDavid09 <benbran009@gmail.com>
2024-05-28 20:41:14 +0300

    Update 1Calculator.py

--- 1Calculator.py
+++ 1Calculator.py
@@ -1,21 +1,32 @@
+c = 0
 
-a = int(input("Pune un numar "))#numar
+while c == 0:
+    a = int(input("Pune un numar: "))  # numar
+    operatie = input("Pune semnul operatiei (+, -, x,:) ")  # operatie
+    b = int(input("Pune al doilea numar: "))  # alt numar
 
-operatie  = input("Pune semnul operatiei ")#operrratie
+    if operatie == "+":
+        print(a, "+", b, "=", a + b)
+    elif operatie == "-":
+        print(a, "-", b, "=", a - b)
+    
+    elif operatie==":": 
+       d = a % b
+       print(a,":",b,"=",a/b,"sau cu rest este",a//b,"si a ramas",d)
+    
+    elif operatie == "x":
+        print(a, "x", b, "=", a * b)
+    else:
+        print("Operatie invalida")
+    
 
-b = int(input("Pune al doilea numar "))#alt numar
-
-
-
-if(operatie== "+"):
-   print(a,"+",b,"=",a+b)
-elif (operatie== "-"):
-   print(a,"-",b,"=",a-b)
-elif (operatie == "x"):
-   print(a,"x",b,"=",a*b)
-
-elif(operatie==":"):
-   d = a % b
-   print(a,":",b,"=",a/b,"sau cu rest este",a//b,"si a ramas",d)
-else:
-   print("Nu ai pus semnul operatiei data viitoare sa pui semnul operatiei")
+    s = 1
+    while s == 1:
+        d = input("Vrei sa iesi? Da/Nu: ")
+        if d.lower() == "da":
+            c = 1
+            s = 0
+        elif d.lower() == "nu":
+            s = 0
+        else:
+            print("Raspuns invalid. Te rog sa introduci 'Da' sau 'Nu'.")
